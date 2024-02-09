372fce53c89d36b8b2e25e362c27144329ae312c
Hyunseo Jung <hyunseo0412@naver.com>
2024-02-09 22:43:03 +0900

    싱글톤 방식의 문제점 해결

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,13 +2,21 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드 10000 -> 20000
-    public void order(String name, int price){
-        System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 여기가 문제! 1만원 넣으면 price가 1만원 되거, 이후 2만원 넣으면 price가 2만원 됨.
-    }
+    //private int price; // 상태를 유지하는 필드 10000 -> 20000
+    //public void order(String name, int price){
+    //    System.out.println("name = " + name + " price = " + price);
+    //    this.price = price; // 여기가 문제! 1만원 넣으면 price가 1만원 되고, 이후 2만원 넣으면 price가 2만원 됨.
+    //}
 
-    public int getPrice(){
+    // 해결 방법: price를 유지하지 말고 받은거 그냥 넘겨주면 됨.
+    //private int price; // 상태를 유지하는 필드 10000 -> 20000
+    public int order(String name, int price){
+        System.out.println("name = " + name + " price = " + price);
+        //this.price = price; // 여기가 문제! 1만원 넣으면 price가 1만원 되고, 이후 2만원 넣으면 price가 2만원 됨.
         return price;
     }
+
+    //public int getPrice(){
+        //return price;
+    //}
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,16 +18,18 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A 사용자 10000원 주문 원래는 멀티스레드 해야 재밌다고 한다..
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB: B 사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자 A 주문 금액 조회
-        int price = statefulService1.getPrice(); // A가 주문하고 금액을 조회하는 사이에 B가 중간에 끼어들어 order를 했다.
-        System.out.println("price = " + price); // 이러면 2만원이 나오게 된다..
+        //int price = statefulService1.getPrice(); // A가 주문하고 금액을 조회하는 사이에 B가 중간에 끼어들어 order를 했다.
+        //System.out.println("price = " + price); // 이러면 2만원이 나오게 된다..
                                                 // 1, 2 두 개가 이름만 다르지 어차피 같은 인스턴스를 쓴다.
                                                 // == 비교하면 똑같음.
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("price = " + userAPrice);
+
+        //assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig{
