9c19588ab499a085edb8d4e52c02a7cba86f1eed
Hyunseo Jung <hyunseo0412@naver.com>
2024-02-12 21:11:26 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient{
 
     private String url;
@@ -27,12 +30,14 @@ public class NetworkClient{
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception { // properties 세팅이 끝나면 (의존관계 주입이 끝나면) 호출해주겠다는 뜻
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() { // 빈이 종료될 때 호출
         System.out.println("NetworkClient.close");
         disconnect();
